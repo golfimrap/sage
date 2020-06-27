7ccfdc840c3a74d045723794f8d80c37ab91bc21
Thanathip Dungpimai <golfimrap@gmail.com>
2020-06-27 17:49:11 +0700

    chang color
    chang color

--- resources/views/fontend/history.blade.php
+++ resources/views/fontend/history.blade.php
@@ -7,7 +7,7 @@
 				<div class="row">
 					<div class="col-xl-12">
 						<div class="hero-cap hero-cap2 text-center pt-80">
-							<h2>History</h2>
+							<h2 style="color: black;">History</h2>
 						</div>
 					</div>
 				</div>
