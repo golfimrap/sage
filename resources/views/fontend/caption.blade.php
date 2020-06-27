ac4a190f5b61335c885700c03ae7b8ccfa207391
Thanathip Dungpimai <golfimrap@gmail.com>
2020-06-27 16:17:51 +0700

    edit h2
    caption gallery history

--- resources/views/fontend/caption.blade.php
+++ resources/views/fontend/caption.blade.php
@@ -7,7 +7,7 @@
 				<div class="row">
 					<div class="col-xl-12">
 						<div class="hero-cap hero-cap2 text-center pt-80">
-							<h2>Instead of thank you</h2>
+							<h2 style="color: black;">Instead of thank you</h2>
 						</div>
 					</div>
 				</div>
