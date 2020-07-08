25d2ae3b984b8b17cb7baadd259fb3865c127817
Thanathip Dungpimai <golfimrap@gmail.com>
2020-07-08 21:02:29 +0700

    update ebook theme and content
    1. theme >>  extends('layouts.template.template')
    2. content >> section('content')

---
+++ resources/views/fontend/ebook.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.template.template')
+@section('content')
+<main>
+	<!-- แก้ไขเนื้อหาจากในนี้ได้เลย -->
+	<div class="slider-area2">
+		<div class="slider-height2 hero-overly d-flex align-items-center">
+			<div class="container">
+				<div class="row">
+					<div class="col-xl-12">
+						<div class="hero-cap hero-cap2 text-center pt-80">
+							<h2 style="color: black;">E-BOOK</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	@include('layouts.template.beforefooter')
+</main>
+@endsection
